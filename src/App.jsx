a27e3217c8ec765f0116717261df29100acdaebd
devikaupendran <devikaupendranr@gmail.com>
2025-03-18 00:58:04 +0530

    added spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Contactus from './pages/Contactus';
+import Spinner from './components/Spinner/Spinner';
 
 const App = () => {
-  return (
-    <div>
-      <>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/contactus' element={<Contactus />} />
-        </Routes>
-      </>
-    </div>
-  )
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const loader = () => {
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 2500);
+        };
+        loader();
+    }, []);
+    return (
+        <div>
+            {
+                isLoading ? (
+                    <Spinner />
+                ) : (
+                    <>
+                        <Navbar />
+                        <Routes>
+                            <Route path='/' element={<Home />} />
+                            <Route path='/contactus' element={<Contactus />} />
+                        </Routes>
+                    </>
+                )
+            }
+        </div>
+    )
 }
 
 export default App
