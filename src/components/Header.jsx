1c0ffc49c14b0c42d2bbc67243732d0fde0e0988
devikaupendran <devikaupendranr@gmail.com>
2025-03-17 21:19:50 +0530

    fixed the issue in animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+
 import { assets } from '../assets/assets'
 import Carousel from './Carousel'
 
 const Header = () => {
     return (
 
-        <div className='w-full h-max pb-10 mb-30'>
+        <div className='w-full h-max pb-10 mb-30 overflow-hidden'>
             <div className='absolute top-0 right-0'>
                 <img src={assets.vector} alt="" width={750} className="circle-animation w-[250px]  md:w-[450px] xl:w-[750px]" />
             </div>
 
             <div className='w-full'>
-                <Carousel/>
+                <Carousel />
             </div>
         </div>
 
