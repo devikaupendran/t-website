5952b70efbeb8d08a17ba30a16875d887c71e40e
devikaupendran <devikaupendranr@gmail.com>
2025-03-18 22:04:56 +0530

    added navigation and close menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,11 @@ const Navbar = () => {
     const padding = location.pathname === '/' ? 'pt-[10px] lg:pt-[55px]' : 'pt-0';
     const boxShadow = location.pathname === '/' ? 'shadow-none' : 'shadow-xl';
 
+    // Function to close the menu
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         // ------------------------------------- navbar -------------------------------------
         <div className={`relative z-10 h-[113px] bg-transparent flex justify-between items-center ${boxShadow} px-6 ${padding}`} >
@@ -26,7 +31,7 @@ const Navbar = () => {
             {/* -------- Options ------  */}
             <div>
                 <ul className={`hidden xl:flex justify-around gap-8 font-semibold ${textColor}`}>
-                    
+
                     <NavLink to={'/'}><li>HOME</li> </NavLink>
                     <NavLink to={'/about-us'}><li>ABOUT US</li></NavLink>
                     <NavLink to={'/services'}> <li>SERVICES</li></NavLink>
@@ -43,32 +48,38 @@ const Navbar = () => {
 
 
             {/* ------ menu options --------  */}
-            <div className={`absolute top-23 left-0 sm:hidden  w-full flex flex-col items-center gap-6 transform transition-transform 
-                ${isMenuOpen ? "opacity-100" : "opacity-0"} bg-white text-black`}
+            <div className={`absolute top-23 left-0  w-full flex flex-col items-center gap-6 transform transition-transform 
+                ${isMenuOpen ? "block" : "hidden"} bg-white text-black`}
                 style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}>
 
-                <NavLink to={'/'} className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>
+                <NavLink to={'/'} onClick={closeMenu}
+                    className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>
                     <li>HOME</li>
                 </NavLink>
 
-                <NavLink to={'/about-us'}>
-                    <li className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>ABOUT US</li>
+                <NavLink to={'/about-us'} onClick={closeMenu}
+                    className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>
+                    <li >ABOUT US</li>
                 </NavLink>
 
-                <NavLink to={'/services'}>
-                    <li className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>SERVICES</li>
+                <NavLink to={'/services'} onClick={closeMenu}
+                    className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>
+                    <li>SERVICES</li>
                 </NavLink>
 
-                <NavLink to={'/products'}>
-                    <li className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>PRODUCTS</li>
+                <NavLink to={'/products'} onClick={closeMenu}
+                    className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>
+                    <li>PRODUCTS</li>
                 </NavLink>
 
-                <NavLink to={'/portfolio'}>
-                    <li className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>PORTFOLIO</li>
+                <NavLink to={'/portfolio'} onClick={closeMenu}
+                    className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>
+                    <li>PORTFOLIO</li>
                 </NavLink>
 
-                <NavLink to={'/contact-us'}>
-                    <li className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>CONTACT US</li>
+                <NavLink to={'/contact-us'} onClick={closeMenu}
+                    className='list-none w-full text-center p-4 hover:bg-orange-400 hover:text-white transition-all cursor-pointer'>
+                    <li>CONTACT US</li>
                 </NavLink>
             </div>
 
